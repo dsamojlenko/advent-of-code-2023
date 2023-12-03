9dec356e53e6e7dda13b913de09ec16b8171c93d
Dave Samojlenko <dsamojlenko@gmail.com>
2023-12-03 17:49:20 -0500

    Initial setup + week one solutions

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { WeekOne } from '@components/WeekOne'
+
+function App() {
+
+  return (
+    <div className="m-8">
+      <div className="bg-white w-full h-screen p-4">
+        <WeekOne />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeekOne.tsx
@@ -0,0 +1,90 @@
+import { weekOneData } from "@data/weekOne";
+
+export const WeekOne = () => {;
+  const input = weekOneData;
+
+  const partOne = () => {
+    const lines = input.split('\n');
+
+    const calVals: string[] = [];
+    lines.map((line) => {
+      const digits = line.match(/\d/g);
+      if (digits) {
+        calVals.push("" + digits[0] + digits[digits.length - 1]);
+      }
+    })
+    
+    const sum = calVals.reduce((acc, curr) => acc + Number(curr), 0);
+
+    return {
+      calibrationValues: calVals,
+      sum: sum
+    }
+  }
+
+  const partTwo = () => {
+    const lines = input.split('\n');
+
+    const numbers: Record<string, number> = {
+      "one": 1, 
+      "two": 2, 
+      "three": 3, 
+      "four": 4, 
+      "five": 5, 
+      "six": 6, 
+      "seven": 7, 
+      "eight": 8, 
+      "nine": 9 
+    };
+    const calVals: string[] = [];
+
+    lines.map((line) => {
+      const row: string[] = [];
+      for (let i = 0; i < line.length; i++) {
+        if (parseInt(line[i]) > 0) {
+          row.push(line[i]);
+        }
+        Object.keys(numbers).map((number) => {
+          const match = line.slice(i, number.length + i);
+          if (match === number) {
+            row.push(String(numbers[number]));
+          }
+        })
+      }
+      calVals.push([row[0], row[row.length - 1]].join(''));
+    })
+
+    const sum = calVals.reduce((acc, curr) => acc + Number(curr), 0);
+
+    return {
+      calibrationValues: calVals,
+      sum: sum
+    }
+  }
+
+  const partOneResult = partOne();
+  const partTwoResult = partTwo();
+
+  return <div className="flex flex-row gap-4">
+    <h1>Week One</h1>
+    <div>
+      <h2>Input:</h2>
+      <pre>{input}</pre>
+    </div>
+    <div>
+      <h2>Calibration values (Part 1)</h2>
+      <pre>{partOneResult.calibrationValues.join("\n")}</pre>
+    </div>
+    <div>
+      <h2>Calibration values (Part 2)</h2>
+      <pre>{partTwoResult.calibrationValues.join("\n")}</pre>
+    </div>
+    <div>
+      <h2>Sum (Part 1)</h2>
+      <pre>{partOneResult.sum}</pre>
+
+      <h2>Sum (Part 2)</h2>
+      <pre>{partTwoResult.sum}</pre>
+    </div>
+  </div>;
+}
